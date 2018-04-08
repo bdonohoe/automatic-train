0ecda479b716ba2566c8b0171e4181e3acc084f5
bdonohoe <bdonohoe@gmail.com>
2018-04-08 11:04:27 -0700

    Update ViewController.swift
    Important commentary.

--- Automatic Train/Automatic Train/ViewController.swift
+++ Automatic Train/Automatic Train/ViewController.swift
@@ -12,12 +12,13 @@ class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // Someone should do something about all this lack of code.
         // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
         didSet {
+        // Fine. I'll do it.
         // Update the view, if already loaded.
         }
     }
