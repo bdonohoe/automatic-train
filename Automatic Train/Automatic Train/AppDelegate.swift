6babda6fd80af27f727bd7ca864da51529133b32
Brendan Donohoe <brendan_donohoe@apple.com>
2018-04-08 11:01:15 -0700

    Initial release!

---
+++ Automatic Train/Automatic Train/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Automatic Train
+//
+//  Created by Brendan Donohoe on 4/8/18.
+//  Copyright © 2018 Brendan Donohoe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Automatic Train/Automatic Train/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Automatic Train
+//
+//  Created by Brendan Donohoe on 4/8/18.
+//  Copyright © 2018 Brendan Donohoe. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
